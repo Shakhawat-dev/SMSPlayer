3561ea614d68e3ca5d90e7674b3a0e49b63e102e
shakhawat <shakhawat.dev@gmail.com>
2024-07-27 19:50:46 +0600

    Initial Commit

---
+++ SMSPlayer/SMSPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMSPlayerApp.swift
+//  SMSPlayer
+//
+//  Created by Md Shakhawat Hossain Shahin on 27/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SMSPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
